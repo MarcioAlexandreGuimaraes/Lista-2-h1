7e98a242a1a27e857252d46e92ae43d3260899c9
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-11-30 22:44:15 -0300

    Add Curso class with properties

---
+++ Curso.cs
@@ -0,0 +1,10 @@
+namespace ListaH1.Domain
+{
+    public class Curso
+    {
+        public int IdCurso { get; set; }
+        public string Nome { get; set; }
+        public string NomeCoordenador { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
