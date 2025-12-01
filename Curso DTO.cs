cade9766f51192b5fcf5692c3d5dea180961234a
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-11-30 22:43:22 -0300

    Create Curso DTO.cs

---
+++ Curso DTO.cs
@@ -0,0 +1,10 @@
+namespace ListaH1.DTOs
+{
+    public class CursoDTO
+    {
+        public int IdCurso { get; set; }
+        public string Nome { get; set; }
+        public string NomeCoordenador { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
